390e713fb906943b1f1463a7f6faedcbd5d19176
long_ <long_@DESKTOP-2VMQVQ7>
2019-05-10 21:04:43 +0900

    Global Arrows Fixed

--- src/main/java/diamond/model/geom/element/diagram/arrow/body/FlipArrowBody.java
+++ src/main/java/diamond/model/geom/element/diagram/arrow/body/FlipArrowBody.java
@@ -7,6 +7,8 @@ package diamond.model.geom.element.diagram.arrow.body;
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D.Double;
 
+import diamond.model.geom.util.DistanceUtil;
+import diamond.model.geom.util.Point2DUtil;
 import diamond.view.screen.draw.style.LineStyle;
 import diamond.view.screen.draw.style.color.OriArrowColor;
 
@@ -15,23 +17,21 @@ import diamond.view.screen.draw.style.color.OriArrowColor;
  *
  */
 public class FlipArrowBody extends AbstractArrowBody {
+    private static final double angle0 = -3 * Math.PI / 4;
     private static final double angle1 = -Math.PI / 4;
-    private static final int r0 = 50;
-    private static final int x = -r0;
-    private static final int y = -r0;
-    private static final int r1 = r0 << 1;
+
     private static final int theta0 = -135;
     private static final int theta = 90;
-    private static final Double p0 = new Double(
-            x + r0 + r1 * Math.cos(Math.toRadians(-theta0)),
-            y + r1 * Math.sin(Math.toRadians(-theta0)));
-    private static final Double p1 = new Double(
-            x + r0 + r1 * Math.cos(Math.toRadians(-theta0 - theta)),
-            y + r1 * Math.sin(Math.toRadians(-theta0 - theta)));
+    private static final Double dir0 = new Double(-Math.sqrt(2) / 2,
+            Math.sqrt(2) / 2);
+    private static final Double dir1 = new Double(Math.sqrt(2) / 2,
+            Math.sqrt(2) / 2);
+    private static final Double dir11 = new Double(1, 1);
+    private static final Double dir01 = new Double(0, 1);
 
     @Override
     public double getAngle0(Double p0, Double p1) {
-        return 0;
+        return angle0;
     }
 
     @Override
@@ -47,17 +47,31 @@ public class FlipArrowBody extends AbstractArrowBody {
     public void draw(Graphics2D g2d, Double p0, Double p1) {
         g2d.setColor(OriArrowColor.ARROW_BODY);
         g2d.setStroke(LineStyle.STROKE_ARROW);
-        g2d.drawOval(x, y, r0 << 1, r0 << 1);
-        g2d.drawArc(x - r0, y - r1, r1 << 1, r1 << 1, theta0, theta);
+        Double o = Point2DUtil.center(p0, p1);
+        double r = DistanceUtil.distance(p0, p1) / 4;
+        Double corner0 = Point2DUtil.sub(o,
+                Point2DUtil.scale(dir11, r));
+        int x = (int) corner0.x;
+        int y = (int) corner0.y;
+        g2d.drawOval(x, y, (int) r * 2, (int) r * 2);
+        g2d.drawArc(x - (int) r, y - (int) r * 2, (int) r * 4, (int) r * 4,
+                theta0, theta);
     }
 
     @Override
     public Double getP0(Double p0, Double p1) {
-        return this.p0;
+        double r = DistanceUtil.distance(p0, p1) / 2;
+        Double o = Point2DUtil.sub(Point2DUtil.center(p0, p1),
+                Point2DUtil.scale(dir01, r / 2));
+
+        return Point2DUtil.plus(o, Point2DUtil.scale(dir0, r));
     }
 
     @Override
     public Double getP1(Double p0, Double p1) {
-        return this.p1;
+        double r = DistanceUtil.distance(p0, p1) / 2;
+        Double o = Point2DUtil.sub(Point2DUtil.center(p0, p1),
+                Point2DUtil.scale(dir01, r / 2));
+        return Point2DUtil.plus(o, Point2DUtil.scale(dir1, r));
     }
 }
--- src/main/java/diamond/model/geom/element/diagram/arrow/body/RotateArrowBody.java
+++ src/main/java/diamond/model/geom/element/diagram/arrow/body/RotateArrowBody.java
@@ -7,6 +7,8 @@ package diamond.model.geom.element.diagram.arrow.body;
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D.Double;
 
+import diamond.model.geom.util.DistanceUtil;
+import diamond.model.geom.util.Point2DUtil;
 import diamond.view.screen.draw.style.LineStyle;
 import diamond.view.screen.draw.style.color.OriArrowColor;
 
@@ -17,11 +19,8 @@ import diamond.view.screen.draw.style.color.OriArrowColor;
 public class RotateArrowBody extends AbstractArrowBody {
     private static final double angle0 = Math.PI / 2;
     private static final double angle1 = -Math.PI / 2;
-    private static final int r = 50;
-    private static final int x = -r;
-    private static final int y = -r;
-    private static final Double p0 = new Double(x + (r << 1), y + r);
-    private static final Double p1 = new Double(x, y + r);
+    private static final Double dir11 = new Double(1, 1);
+    private static final Double dir10 = new Double(1, 0);
 
     @Override
     public double getAngle0(Double p0, Double p1) {
@@ -41,16 +40,26 @@ public class RotateArrowBody extends AbstractArrowBody {
     public void draw(Graphics2D g2d, Double p0, Double p1) {
         g2d.setColor(OriArrowColor.ARROW_BODY);
         g2d.setStroke(LineStyle.STROKE_ARROW);
-        g2d.drawOval(x, y, r << 1, r << 1);
+        Double o = Point2DUtil.center(p0, p1);
+        double r = DistanceUtil.distance(p0, p1) / 4;
+        Double corner0 = Point2DUtil.sub(o,
+                Point2DUtil.scale(dir11, r));
+        int x = (int) corner0.x;
+        int y = (int) corner0.y;
+        g2d.drawOval(x, y, (int) r * 2, (int) r * 2);
     }
 
     @Override
     public Double getP0(Double p0, Double p1) {
-        return this.p0;
+        double r = DistanceUtil.distance(p0, p1) / 4;
+        Double o = Point2DUtil.center(p0, p1);
+        return Point2DUtil.sub(o, Point2DUtil.scale(dir10, r));
     }
 
     @Override
     public Double getP1(Double p0, Double p1) {
-        return this.p1;
+        double r = DistanceUtil.distance(p0, p1) / 4;
+        Double o = Point2DUtil.center(p0, p1);
+        return Point2DUtil.plus(o, Point2DUtil.scale(dir10, r));
     }
 }
