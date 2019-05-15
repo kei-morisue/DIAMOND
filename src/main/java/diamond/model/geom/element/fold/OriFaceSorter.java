f4bc471fefb29d641b7e2bdeaa279e7d1e05badb
long_ <long_@DESKTOP-2VMQVQ7>
2019-05-15 12:59:57 +0900

    refactoring

--- src/main/java/diamond/model/geom/element/fold/OriFaceSorter.java
+++ src/main/java/diamond/model/geom/element/fold/OriFaceSorter.java
@@ -19,14 +19,15 @@ public class OriFaceSorter {
 
     public static LinkedList<OriFace> sort(LinkedList<OriFace> faces) {
         debugCount = 0;
-        validate(faces);
+        while (!validate(faces)) {
+            debugCount++;
+        }
         return faces;
     }
 
-    private static void validate(LinkedList<OriFace> faces) {
-        debugCount++;
-        if (debugCount > 5000) {
-            return;
+    private static boolean validate(LinkedList<OriFace> faces) {
+        if (debugCount > 1000) {
+            return true;
         }
         for (int i = 0; i < faces.size() - 1; i++) {
             for (int j = faces.size() - 1; j > i; j--) {
@@ -34,16 +35,14 @@ public class OriFaceSorter {
                 OriFace f1 = faces.get(j);
                 if (OriFaceComparator.tryCompare(f0, f1, null) == 1) {
                     swap(i, j, faces);
-                    validate(faces);
-                    return;
+                    return false;
                 }
                 if (validateCollision(i, j, f0, f1, faces)) {
-                    validate(faces);
-                    return;
+                    return false;
                 }
             }
         }
-        return;
+        return true;
     }
 
     public static boolean validateCollision(int i, int j, OriFace f0,
