10cd20d62da3ad06508bf4c1b01334471c941e3f
long_ <long_@172.16.0.101>
2020-08-12 21:21:16 +0900

    #36

---
+++ src/main/java/diamond/model/cyborg/diagram/step/Folder.java
@@ -0,0 +1,13 @@
+/**
+ * DIAMOND - Origami Diagram Editor
+ * Copyright (C) 2018-2021 Kei Morisue
+ */
+package diamond.model.cyborg.diagram.step;
+
+/**
+ * @author Kei Morisue
+ *
+ */
+public class Folder {
+
+}
