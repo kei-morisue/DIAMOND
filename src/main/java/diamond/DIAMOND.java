d35cb90572abf7be1f7451f2edd9da9c3bb0a420
kei <morisue.kei@gmail.com>
2023-07-28 16:29:50 +0900

    ui laf

--- src/main/java/diamond/DIAMOND.java
+++ src/main/java/diamond/DIAMOND.java
@@ -1,11 +1,26 @@
 package diamond;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+
 import diamond.view.ui.frame.MainFrame;
 
 public class DIAMOND {
 
-	public static void main(String[] args) {
+	public static void main(
+			String[] args) {
+		setUILaf();
 		new MainFrame();
+	}
+
+	private static void setUILaf() {
+		try {
+			UIManager.setLookAndFeel(new FlatDarkLaf());
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
